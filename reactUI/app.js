b50cd83e65e3cfdc48716ac0f3de1b5df94973aa
atkinsta <atkinstaa@gmail.com>
2019-04-10 20:47:49 -0400

    Added a phone GUI in the game, styled

--- reactUI/app.js
+++ reactUI/app.js
@@ -1,73 +1,14 @@
 import "./styles/main.scss"
+import Phone from "./components/Phone.js"
 
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
-const styles = {
-    position: "relative",
-    width: "30%",
-    height: "40vh",
-    margin: "20vh auto",
-    borderRadius: "10px",
-    boxShadow: "1px 1px 2px black",
-    backgroundColor: "black",
-    color: "white"
-};
-
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            time: new Date(),
-            message: 'default message',
-            carSpawns: ["0, 1, 3"],
-            show: false
-        }
-    }
-
-    componentDidMount() {
-        EventManager.addHandler('onMessage', this.onMessage.bind(this));
-        EventManager.addHandler("onKeyPress", this.onKeyPress.bind(this));
-        EventManager.addHandler("carSpawnAdded", this.carSpawnAdded.bind(this));
-        this.timerId = setInterval(() => {
-            this.setState({time: new Date()});
-        }, 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerId);
-        EventManager.removeHandler("carSpawnAdded", this.carSpawnAdded);
-        EventManager.removeHandler('onMessage', this.onMessage);
-        EventManager.removeHandler("onKeyPress", this.onKeyPress);
-    }
-
-    onMessage(value) {
-        this.setState({message: value})
-    }
-
-    onKeyPress() {
-        this.setState({show: !this.state.show});
-    }
-
-    carSpawnAdded(position) {
-        this.setState({carSpawns: [...this.state.carSpawns, position]});
-        let retPos = position;
-        mp.trigger("displaySpawns", retPos);
-    }
-
-    // send current url to client
-     click() {
-        let currentUrl = window.location.pathname;
-        mp.trigger("showUrl", currentUrl);
-        this.state.carSpawns.forEach(spawn => {
-            mp.trigger("displaySpawns", spawn);
-        })
-    }
-
     render() {
         return(
-            <div style={this.styles}>
-                <img src={"./assets/Phone.jpg"} alt={"A phone"}/>
+            <div className={"main"}>
+                <Phone />
             </div>
         )
     }
--- reactUI/components/Phone.js
+++ reactUI/components/Phone.js
@@ -1,13 +1,64 @@
 import React, {Component} from "react";
+import "../styles/main.scss"
 
 class Phone extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date(),
+            message: 'default message',
+            carSpawns: ["0, 1, 3"],
+            show: false
+        }
+    }
 
+    componentDidMount() {
+        EventManager.addHandler('onMessage', this.onMessage.bind(this));
+        EventManager.addHandler("onKeyPress", this.onKeyPress.bind(this));
+        EventManager.addHandler("carSpawnAdded", this.carSpawnAdded.bind(this));
+        this.timerId = setInterval(() => {
+            this.setState({time: new Date()});
+        }, 1000);
+    }
 
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+        EventManager.removeHandler("carSpawnAdded", this.carSpawnAdded);
+        EventManager.removeHandler('onMessage', this.onMessage);
+        EventManager.removeHandler("onKeyPress", this.onKeyPress);
+    }
+
+    onMessage(value) {
+        this.setState({message: value})
+    }
+
+    onKeyPress() {
+        this.setState({show: !this.state.show});
+    }
+
+    carSpawnAdded(position) {
+        this.setState({carSpawns: [...this.state.carSpawns, position]});
+        mp.trigger("displaySpawns", position);
+    }
+
+    // send current url to client
+    click() {
+        let currentUrl = window.location.pathname;
+        mp.trigger("showUrl", currentUrl);
+        this.state.carSpawns.forEach(spawn => {
+            mp.trigger("displaySpawns", spawn);
+        })
+    }
     render() {
         return(
-            <div>
+            <div className={"app"}>
+                <img className={"phoneImg"} src={"./assets/Phone.png"} alt={"A phone"} style={{maxHeight: "100%", maxWidth:"100%"}}/>
+                <div className={"phoneHome"}>
 
+                </div>
             </div>
         )
     }
 }
+
+export default Phone;
